51d6712594442eda7017cb6e82f67073038feff5
Shahriar <shahriarh655@gmail.com>
2024-11-11 20:33:54 +0600

    Implemented all tasks component into the admin dashboard.

--- src/components/dashboard/AdminDashboard.jsx
+++ src/components/dashboard/AdminDashboard.jsx
@@ -1,13 +1,13 @@
 import AdminForm from "./dashComponents/adminForm/AdminForm";
+import AllTasks from "./dashComponents/AllTasks";
 import Header from "./dashComponents/Header";
 
 const AdminDashboard = () => {
   return (
-    <div className="w-full h-screen bg-slate-900 p-8">
-      <div>
-        <Header />
-        <AdminForm />
-      </div>
+    <div className="w-full min-h-screen bg-slate-900 p-8">
+      <Header />
+      <AdminForm />
+      <AllTasks />
     </div>
   );
 };
---
+++ src/components/dashboard/dashComponents/AllTasks.jsx
@@ -0,0 +1,33 @@
+const AllTasks = () => {
+  return (
+    <div className="w-full max-h-72  bg-slate-950 rounded-md mt-8 p-4 space-y-2 overflow-auto">
+      <div className="w-full h-full  bg-green-500 flex justify-between items-center p-3">
+        <h1>Wilson</h1>
+        <h2>Create A Blog APP</h2>
+        <p>Status</p>
+      </div>
+      <div className="w-full h-full bg-yellow-500 flex justify-between items-center p-3">
+        <h1>Wilson</h1>
+        <h2>Create A Blog APP</h2>
+        <p>Status</p>
+      </div>
+      <div className="w-full h-full bg-blue-500 flex justify-between items-center p-3">
+        <h1>Wilson</h1>
+        <h2>Create A Blog APP</h2>
+        <p>Status</p>
+      </div>
+      <div className="w-full h-full bg-red-500 flex justify-between items-center p-3">
+        <h1>Wilson</h1>
+        <h2>Create A Blog APP</h2>
+        <p>Status</p>
+      </div>
+      <div className="w-full h-full bg-purple-500 flex justify-between items-center p-3">
+        <h1>Wilson</h1>
+        <h2>Create A Blog APP</h2>
+        <p>Status</p>
+      </div>
+    </div>
+  );
+};
+
+export default AllTasks;
