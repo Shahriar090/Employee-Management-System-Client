279f4fbe701754e4e8d2120630f3e64ad30ca211
Shahriar <shahriarh655@gmail.com>
2024-11-12 19:41:14 +0600

    Set and get admin and employee data in local storage.

---
+++ src/utils/localStorage.js
@@ -0,0 +1,12 @@
+import employeesData from "../data/employee.json";
+import adminData from "../data/admin.json";
+
+export const setLocalStorage = () => {
+  localStorage.setItem("employees", JSON.stringify(employeesData));
+  localStorage.setItem("admins", JSON.stringify(adminData));
+};
+
+export const getLocalStorage = () => {
+  const employees = JSON.parse(localStorage.getItem("employees"));
+  const admins = JSON.parse(localStorage.getItem("admins"));
+};
